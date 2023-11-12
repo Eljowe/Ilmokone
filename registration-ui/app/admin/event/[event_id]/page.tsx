4bb9d7e5b3a718fe29f5fa6e15d19e692b26f560
Joel <joelwickstroem@gmail.com>
2023-11-12 21:48:31 +0200

    admin events

---
+++ registration-ui/app/admin/event/[event_id]/page.tsx
@@ -0,0 +1,129 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Button, FieldError, Form, Input, Label, TextField } from 'react-aria-components';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+  params: {
+    event_id: string;
+  };
+};
+
+type Inputs = {
+  name: string;
+  email: string;
+};
+
+interface Event {
+  participants?: Array<string>;
+  description: string;
+  title: string;
+  date: string;
+  registration_start: string;
+  event_location?: string;
+  maximum_participants?: number;
+}
+
+export default function Event({ params }: Props) {
+  const [authorized, setAuthorized] = useState<Boolean>(false);
+  const slug = params.event_id;
+  const [event, setEvent] = useState<Event | null>(null);
+
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    try {
+      fetch('/api/auth').then(response => {
+        if (response.status == 200) {
+          setAuthorized(true);
+        } else {
+          router.push('/login');
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    try {
+      fetch(`/api/event/${slug}`)
+        .then(response => response.json())
+        .then(response => {
+          setEvent({
+            title: response[0].title,
+            description: response[0].event_description,
+            date: response[0].event_date,
+            registration_start: response[0].registration_starts,
+            event_location: response[0].event_location,
+          });
+        });
+    } catch (e) {
+      return;
+    }
+  }, [slug, router]);
+
+  if (authorized) {
+    return (
+      <main className="flex min-h-screen w-screen flex-col items-center justify-start p-4">
+        <h1 className="text-2xl font-bold">Admin view</h1>
+        {event ? (
+          <div className="flex h-full w-full max-w-[1200px] flex-col items-center justify-center space-y-10 divide-y-2 rounded-xl bg-gray-50 px-4 py-20 sm:px-10">
+            <h1 className="text-2xl font-bold">{event.title}</h1>
+            <div className="flex w-full flex-col justify-center space-y-4 pt-10 sm:flex-row sm:space-x-4 sm:space-y-0">
+              <div className="w-max-[800px] flex flex-col space-y-4 rounded-xl hover:border hover:border-blue-500 sm:w-full sm:min-w-[300px]">
+                <p>{event.description}</p>
+              </div>
+              <div className="flex w-min flex-col justify-between rounded-xl border border-zinc-400 p-4 hover:border-blue-500 sm:w-full">
+                <span>When: {event.date}</span>
+                <span>Where: {event.event_location}</span>
+                <span>Registration starts: {event.date}</span>
+              </div>
+            </div>
+
+            <div className="flex w-full items-center justify-center">
+              <Form onSubmit={handleSubmit(onSubmit)} className="mx-auto flex w-[300px] flex-col space-y-4 pt-10">
+                <TextField name="name" isRequired className="flex flex-col">
+                  <Label>Name</Label>
+                  <Input
+                    placeholder="Name"
+                    {...register('name')}
+                    className="focus:shadow-outline w-full appearance-none rounded border border-zinc-300  px-4 py-2 leading-tight text-gray-700 transition duration-150 ease-in-out focus:border-blue-400 focus:outline-none"
+                  />
+                  <FieldError />
+                </TextField>
+                <TextField name="email" type="email" isRequired className="flex flex-col">
+                  <Label>Email</Label>
+                  <Input
+                    placeholder="user@email.com"
+                    {...register('email')}
+                    className="focus:shadow-outline w-full appearance-none rounded border border-zinc-300  px-4 py-2 leading-tight text-gray-700 transition duration-150 ease-in-out focus:border-blue-400 focus:outline-none"
+                  />
+                  <FieldError />
+                </TextField>
+                <Button
+                  type="submit"
+                  className="cursor w-20 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-400"
+                >
+                  Submit
+                </Button>
+              </Form>
+            </div>
+            {event?.participants && <p>Participants</p>}
+          </div>
+        ) : (
+          <h1>No event</h1>
+        )}
+      </main>
+    );
+  }
+}
--- registration-ui/app/admin/page.tsx
+++ registration-ui/app/admin/page.tsx
@@ -10,6 +10,7 @@ interface Event {
   registration_start: string;
   event_location?: string;
   maximum_participants?: number;
+  id: number;
 }
 
 export default function Page() {
@@ -32,6 +33,7 @@ export default function Page() {
                 registration_start: event.registration_start,
                 event_location: event.event_location,
                 maximum_participants: event.maximum_participants,
+                id: event.id,
               }));
               setEvents(events);
             });
@@ -48,21 +50,24 @@ export default function Page() {
     return (
       <main className="flex min-h-screen w-screen items-start justify-center p-4">
         <div className="flex h-full w-full max-w-[1200px] flex-col items-center justify-center space-y-10 divide-y-2 rounded-xl bg-gray-50 px-4 py-20 sm:px-10">
-          <div>
-            <h1>Hello</h1>
-            <p>You are authorized</p>
-          </div>
-          {events &&
-            events.map(event => (
-              <div key={event.title} className="flex w-full">
-                <div className="mx-auto flex w-full max-w-[500px] flex-row justify-between">
-                  <h1>{event.title}</h1>
-                  <button className="cursor w-20 transform rounded bg-blue-500 px-4 py-2 text-black duration-500 hover:bg-blue-400">
+          <h1 className="text-2xl">Events control panel</h1>
+          <div className="flex w-full flex-col space-y-4 pt-4">
+            {events &&
+              events.map(event => (
+                <div
+                  key={event.title}
+                  className="mx-auto flex w-full max-w-[500px] flex-row justify-between rounded border border-zinc-400 p-2"
+                >
+                  <h1 className="my-auto">{event.title}</h1>
+                  <a
+                    href={`/admin/event/${event.id}`}
+                    className="cursor w-20 transform rounded bg-blue-500 px-4 py-2 text-center text-white duration-500 hover:bg-blue-400"
+                  >
                     Edit
-                  </button>
+                  </a>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
       </main>
     );
